667a171c628a25e083194549a16ac6cb02114434
mpropl <mpropl@gmail.com>
2019-09-13 19:11:08 +0200

    first commit

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function calculateResult() {
   if(wynik_wyliczen >= 1){
     show_result(wynik_wyliczen);
     dialogend();
-  }if(wynik_wyliczen === Infinity || wynik_wyliczen < 0 || wynik_wyliczen === -Infinity ){
+  }if(wynik_wyliczen === Infinity || wynik_wyliczen < -1 || wynik_wyliczen === -Infinity ){
     dialogproblem();
     return;
 
