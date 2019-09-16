853e4f4e5f50223656baad24ad61a4f1c58db889
mpropl <mpropl@gmail.com>
2019-09-16 11:16:53 +0200

    poprawki lukas

--- script.js
+++ script.js
@@ -22,14 +22,11 @@ function searchForElements() {
   resulthtml = document.getElementById("result");
   dialog = document.getElementById("speech-bubble");
   input = document.querySelectorAll("input")
-  
 }
 
 function prepareDOMEvents() {
   addBtn.addEventListener("click", calculateResult,stopInterval);
-  
 }
-
 
 function iliterateInput(){
   for (let i = 0; i < input.length; i++ ){
@@ -37,11 +34,11 @@ function iliterateInput(){
   }
 defaultColor = setInterval(iliterateInputDefault, 4000);
 }
+
 function iliterateInputDefault(){
   for (let i = 0; i < input.length; i++ ){
     input[i].style.border = "1px grey solid"
   }
-
 }
 
 function dialogm(){
@@ -49,7 +46,6 @@ function dialogm(){
   dialog.innerHTML = `<h1>Hej kolego/koleżanko</h1>
   <h2>chyba musisz coś wpisać !!</h2>`;
   dialog.style.display ="block";
-  
 }
 
 function dialogstart(){
@@ -57,109 +53,98 @@ function dialogstart(){
   dialog.innerHTML = `<h1>Nie ma na co czekać,</h1>
   <h2>uzupełnij wszystkie pola.</h2>`;
   dialog.style.display ="block";
- 
 }
+
 function dialogproblem(){
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>Hej,</h1>
   <h2>chyba coś ściemniasz.</h2>`;
   dialog.style.display ="block";
- 
 }
+
 function dialogend(){
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>No i teraz</h1>
   <h2>wszystko jasne.</h2>`;
   dialog.style.display ="block";
- 
 }
 
 function dialognone(){
    dialog.style.display ="none";
-   
 }
+
 function stopInterval(){
   clearInterval(dialognone)
 }
+
 function validateLoop(input){
   for (let i = 0; i < input.length; i++ ){
     input[i].value
     return
   }
-  debugger;
 }
 
 function validateForm(
-  age_input,
-  saving_input,
-  retirement_input,
-  death_input,
-  pension_input
-)
-
- {
-  
-  if (
-    (age_input,
-      saving_input,
-      retirement_input,
-      death_input,
-      pension_input === Number())
+  ageInput,
+  savingInput,
+  retirementInput,
+  deathInput,
+  pensionInput
+){
+    if (
+    (ageInput,
+      savingInput,
+      retirementInput,
+      deathInput,
+      pensionInput === Number())
   ) {
     calculateResult();
-    
+
   } else if (
-    (age_input,
-    saving_input,
-    retirement_input,
-    death_input,
-    pension_input === "")
+    (ageInput,
+      savingInput,
+      retirementInput,
+      deathInput,
+      pensionInput === "")
   ) {
     dialogm();
     iliterateInput();
   }
 }
+
 function calculateResult() {
-  let age_input = age.value;
-  let saving_input = saving.value;
-  let retirement_input = retirement.value;
-  let death_input = death.value;
-  let pension_input = pension.value;
+  let ageInput = age.value;
+  let savingInput = saving.value;
+  let retirementInput = retirement.value;
+  let deathInput = death.value;
+  let pensionInput = pension.value;
 
   validateForm(
-    age_input,
-    saving_input,
-    retirement_input,
-    death_input,
-    pension_input
+    ageInput,
+    savingInput,
+    retirementInput,
+    deathInput,
+    pensionInput
   );
 
-  let czas_na_skladki =
-    (Math.floor(retirement_input) - Math.floor(age_input)) * Math.floor(12);
-  let ile_wyplaty =
-    (Math.floor(death_input) - Math.floor(retirement_input)) * Math.floor(12);
-  let oszczednosci = Math.floor(ile_wyplaty) * Math.floor(pension_input);
-  let ile_do_oszczedzenia = Math.floor(oszczednosci) - Math.floor(saving_input);
-  let wynik_wyliczen =
-    Math.floor(ile_do_oszczedzenia) / Math.floor(czas_na_skladki);
+  let timeToRetire = (Math.floor(retirementInput) - Math.floor(ageInput)) * Math.floor(12);
+  let retiredYears = (Math.floor(deathInput) - Math.floor(retirementInput)) * Math.floor(12);
+  let lifeSavings = Math.floor(retiredYears) * Math.floor(pensionInput);
+  let savingTime = Math.floor(lifeSavings) - Math.floor(savingInput);
+  let resultOfEquations = Math.floor(savingTime) / Math.floor(timeToRetire);
 
-  console.log(Math.floor(wynik_wyliczen));
-  
-  
-  if(wynik_wyliczen >= 1){
-    show_result(wynik_wyliczen);
+  console.log(Math.floor(resultOfEquations));
+  if(resultOfEquations >= 1){
+    show_result(resultOfEquations);
     dialogend();
-  }if(wynik_wyliczen === Infinity || wynik_wyliczen === -Infinity ){
+  }if(resultOfEquations === Infinity || resultOfEquations === -Infinity ){
     dialogproblem();
     return;
-
      }
-  
   }
 
-function show_result(wynik_wyliczen) {
-  resulthtml.innerHTML = +Math.floor(wynik_wyliczen) + " zł" + "<br>"+ " miesięcznie";
+function show_result(resultOfEquations) {
+  resulthtml.innerHTML =+ Math.floor(resultOfEquations) + " zł" + "<br>"+ " miesięcznie";
   }
-  
 
 document.addEventListener("DOMContentLoaded", main);
