9620a34618abdc28756852a675584d0a57d3cabb
mpropl <mpropl@gmail.com>
2019-09-18 12:28:54 +0200

    validate

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let addBtn;
 let resulthtml;
 let dialog;
 let input;
+let inputsWithError = []
 
 function main() {
   searchForElements();
@@ -25,74 +26,75 @@ function searchForElements() {
 }
 
 function prepareDOMEvents() {
-  addBtn.addEventListener("click", calculateResult,stopInterval);
+  addBtn.addEventListener("click", calculateResult);
 }
 
-function iliterateInput(){
-  for (let i = 0; i < input.length; i++ ){
-    input[i].style.border = "1px red solid"
+function iliterateInput() {
+  for (let i = 0; i < inputsWithError.length; i++) {
+    inputsWithError[i].style.border = "1px red solid"
   }
-defaultColor = setInterval(iliterateInputDefault, 4000);
+
+  defaultColor = setInterval(iliterateInputDefault, 4000)
+
 }
 
-function iliterateInputDefault(){
-  for (let i = 0; i < input.length; i++ ){
+function iliterateInputDefault() {
+  for (let i = 0; i < input.length; i++) {
     input[i].style.border = "1px grey solid"
   }
 }
 
-function dialogm(){
+function dialogm() {
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>Hej kolego/koleżanko</h1>
   <h2>chyba musisz coś wpisać !!</h2>`;
-  dialog.style.display ="block";
+  dialog.style.display = "block";
 }
 
-function dialogstart(){
+function dialogstart() {
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>Nie ma na co czekać,</h1>
   <h2>uzupełnij wszystkie pola.</h2>`;
-  dialog.style.display ="block";
+  dialog.style.display = "block";
 }
 
-function dialogproblem(){
+function dialogproblem() {
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>Hej,</h1>
   <h2>chyba coś ściemniasz.</h2>`;
-  dialog.style.display ="block";
+  dialog.style.display = "block";
 }
 
-function dialogend(){
+function dialogend() {
   dialognone = setInterval(dialognone, 5000);
   dialog.innerHTML = `<h1>No i teraz</h1>
   <h2>wszystko jasne.</h2>`;
-  dialog.style.display ="block";
+  dialog.style.display = "block";
 }
 
-function dialognone(){
-   dialog.style.display ="none";
+function dialognone() {
+  dialog.style.display = "none";
 }
 
-function stopInterval(){
+function stopInterval() {
   clearInterval(dialognone)
 }
 
-function validateLoop(input){
-  for (let i = 0; i < input.length; i++ ){
-
-    if (input[i].value === ''){
-      dialogm();
-      iliterateInput();
-    }else if(input[i].value === Number()) {
-      calculateResult();
+function validateLoop(input) {
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].value === '') {
+      inputsWithError.push((input[i]))
     }
+
   }
+  iliterateInput();
+  dialogm();
 }
 
-
 function calculateResult() {
+  stopInterval();
   validateLoop(input);
-  
+
   let ageInput = age.value;
   let savingInput = saving.value;
   let retirementInput = retirement.value;
@@ -106,17 +108,18 @@ function calculateResult() {
   let resultOfEquations = Math.floor(savingTime) / Math.floor(timeToRetire);
 
   console.log(Math.floor(resultOfEquations));
-  if(resultOfEquations >= 1){
+  if (resultOfEquations >= 1) {
     show_result(resultOfEquations);
     dialogend();
-  }if(resultOfEquations === Infinity || resultOfEquations === -Infinity ){
+  }
+  if (resultOfEquations === Infinity || resultOfEquations === -Infinity) {
     dialogproblem();
     return;
-     }
   }
+}
 
 function show_result(resultOfEquations) {
-  resulthtml.innerHTML =+ Math.floor(resultOfEquations) + " zł" + "<br>"+ " miesięcznie";
-  }
+  resulthtml.innerHTML = +Math.floor(resultOfEquations) + " zł" + "<br>" + " miesięcznie";
+}
 
 document.addEventListener("DOMContentLoaded", main);
